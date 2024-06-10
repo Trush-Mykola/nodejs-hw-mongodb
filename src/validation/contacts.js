0e0db79939c1ce30aa2ee38861fd96739ebaf377
Mykola Trush <tryshmykola@gmail.com>
2024-06-10 14:27:25 +0300

    edit validation schema

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -5,7 +5,7 @@ export const createContactSchema = Joi.object({
   phoneNumber: Joi.string().required().min(3).max(20),
   email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().required().valid('work', 'home', 'personal'),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
 
 export const updateContactSchema = Joi.object({
