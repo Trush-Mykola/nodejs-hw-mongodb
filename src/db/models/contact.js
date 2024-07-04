6fffc4ab2b20cb9e785f7e96310804327a4f1e3a
Mykola Trush <tryshmykola@gmail.com>
2024-07-04 20:29:05 +0300

    fix post yaml

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -5,7 +5,11 @@ const contactSchema = new Schema(
     name: { type: String, required: true },
     phoneNumber: { type: String, required: true },
     email: { type: String },
-    isFavourite: { type: Boolean, default: false },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+      set: (v) => v === 'true' || v === true,
+    },
     contactType: {
       type: String,
       required: true,
